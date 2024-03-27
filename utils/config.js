635dce7e508039e93cc6aae68362bad53952507c
Darkusmaley <daleymarcus091@gmail.com>
2024-03-27 00:55:11 -0400

    refactored/app.js,.gitignore,package.json,cinfigs.js

--- utils/config.js
+++ utils/config.js
@@ -1,1 +1,15 @@
-module.exports.JWT_SECRET = "Secret_Key_Here";
+const jwt = require("jsonwebtoken");
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const generateToken = (user) => {
+  const token = jwt.sign(
+    { _id: user._id },
+    NODE_ENV === "production" ? JWT_SECRET : "Secret_Key_Here",
+    { expiresIn: "7d" },
+  );
+
+  return token;
+};
+
+module.exports = { NODE_ENV, JWT_SECRET, generateToken };
