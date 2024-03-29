cd656dce8b5e29e43165be9b89780998d7f537c3
Darkusmaley <daleymarcus091@gmail.com>
2024-03-28 22:52:29 -0400

    changed actar to imageUrl in validation.js

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -16,7 +16,7 @@ module.exports.validateItemCreation = celebrate({
       "string.empty": 'The "name" field must be filled in',
     }),
 
-    avatar: Joi.string().required().custom(validateURL).messages({
+    imageUrl: Joi.string().required().custom(validateURL).messages({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'the "imageUrl" field must be a valid url',
     }),
